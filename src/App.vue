782532a4d4dde2aca5166463534d9d773a5791ad
zhuyingjie <m6567fc@outlook.com>
2020-11-14 17:23:54 +0800

    add scripts

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <img src="@/assets/img/favicon.ico"  alt="favicon"/>
+    <img src="@/assets/img/favicon.ico" alt="favicon" />
   </div>
 </template>
 <script lang="ts">
-import { Vue, Options} from "vue-class-component"
+import { Vue, Options } from "vue-class-component";
 
 @Options({
-  name: 'App'
+  name: "App",
 })
-export default class App extends Vue{
-  title = '标题2'
+export default class App extends Vue {
+  title = "标题2";
 
   // 组件方法也可以直接声明为实例的方法
   onClick(): void {
     window.alert(this.title);
   }
-};
+}
 </script>
